26b5e88fb13bacef32d26bfe4a29d1d4e8b5e8b3
Luqman <bluebearymuffin55@gmail.com>
2023-04-17 17:02:59 +0800

    Create function that randomly select from 3 option

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
+let choices = "";
+let computerSelection = getComputerChoice();
 
+function getComputerChoice () {
+    const randomChoices = (Math.floor(Math.random() * 3) + 1);
+
+    if (randomChoices <= 1) {
+       return choices = "rock";
+    }else if (randomChoices <= 2) {
+       return choices = "paper";
+    }else if (randomChoices <= 3) {
+       return choices = "scissors";
+    }
+}
+console.log(`The computer choose: ${computerSelection}`);
